274f2fca5a6d174e2c95be752b4b2953cf9f9377
Ahmed-Ehab99 <ahmedehab1677@gmail.com>
2025-01-01 19:18:21 +0200

    My Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from "react-router-dom"
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Projects, StarsCanvas, Tech } from './components';
-// import Projects from "./components/Projects";
 
 const App = () => {
   return (
